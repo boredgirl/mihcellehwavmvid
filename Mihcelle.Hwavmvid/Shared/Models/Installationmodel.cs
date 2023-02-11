1f9ebedf072b86e4ddbc016c2cec5aef41e82d22
boredgirl <hwavmvid.17@gmail.com>
2023-02-11 18:04:55 +0100

    added installtion model class

---
+++ Mihcelle.Hwavmvid/Shared/Models/Installationmodel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mihcelle.Hwavmvid.Shared.Models
+{
+    public class Installationmodel
+    {
+
+        public string Sqlserverinstance { get; set; } = @".\sqlexpress";
+        public string Databasename { get; set; } = "mihcelle.hwavmvid";
+        public string Databaseowner { get; set; } = "sa";
+        public string Databaseownerpassword { get; set; } = "!default";
+        public string Hostusername { get; } = "host";
+        public string Hostpassword { get; set; } = "!administrator";
+        public bool Usessl { get; } = true;
+
+    }
+}
