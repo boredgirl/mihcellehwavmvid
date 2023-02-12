af5f9a56e971c2687714eee0749d041b6ad32b7f
boredgirl <hwavmvid.17@gmail.com>
2023-02-12 13:16:54 +0100

    updated installationcontroller

--- Mihcelle.Hwavmvid/Server/Controllers/InstallationController.cs
+++ Mihcelle.Hwavmvid/Server/Controllers/InstallationController.cs
@@ -13,6 +13,7 @@ using System.Text.Encodings.Web;
 using System.Text.Json.Serialization;
 using Mihcelle.Hwavmvid.Server.Data;
 using Microsoft.EntityFrameworkCore;
+using Mihcelle.Hwavmvid.Shared.Constants;
 
 namespace Mihcelle.Hwavmvid.Server.Controllers
 {
@@ -25,14 +26,16 @@ namespace Mihcelle.Hwavmvid.Server.Controllers
         public IConfiguration configuration { get; set; }
         public UserManager<Applicationuser> usermanager { get; set; }
         public SignInManager<Applicationuser> signinmanager { get; set; }
+        public RoleManager<IdentityRole> rolemanager { get; set; }
         public Applicationdbcontext context { get; set; }
 
-        public InstallationController(IWebHostEnvironment environment, IConfiguration configuration, UserManager<Applicationuser> usermanager, SignInManager<Applicationuser> signinmanager, Applicationdbcontext context)
+        public InstallationController(IWebHostEnvironment environment, IConfiguration configuration, UserManager<Applicationuser> usermanager, SignInManager<Applicationuser> signinmanager, RoleManager<IdentityRole> rolemanager, Applicationdbcontext context)
         {
             this.iwebhostenvironment = environment;
             this.configuration = configuration;
             this.usermanager = usermanager;
             this.signinmanager = signinmanager;
+            this.rolemanager = rolemanager;
             this.context = context;
         }
 
@@ -57,6 +60,29 @@ namespace Mihcelle.Hwavmvid.Server.Controllers
             this.context.Database.SetConnectionString(connectionstring);
             await this.context.Database.EnsureCreatedAsync();
             await this.context.Database.MigrateAsync();
+
+            var applicationuser = new Applicationuser();
+            applicationuser.UserName = model.Hostusername;
+            applicationuser.Email = model.Hostusername + "@mihcelle.hwavmvid.com";
+            applicationuser.PasswordHash = model.Hostpassword;
+            applicationuser.EmailConfirmed = true;
+            applicationuser.TwoFactorEnabled = false;
+            applicationuser.LockoutEnabled = true;
+
+            var createuserresult = await this.usermanager.CreateAsync(applicationuser, model.Hostpassword);
+            if (createuserresult.Succeeded)
+            {
+                if (!await this.rolemanager.RoleExistsAsync(Authentication.Administratorrole))
+                {
+                    await this.rolemanager.CreateAsync(new IdentityRole(Authentication.Userrole));
+                }
+
+                var addtoroleresult = await usermanager.AddToRoleAsync(applicationuser, Authentication.Userrole);
+                if (!addtoroleresult.Succeeded)
+                {
+                    throw new HubException("Failed to add user to role..");
+                }
+            }
         }
 
         public void Updatedconnectionstring(string connectionstring)
@@ -66,10 +92,7 @@ namespace Mihcelle.Hwavmvid.Server.Controllers
             if (deserializedconfig != null)
             {
                 deserializedconfig["ConnectionStrings"] = new { DefaultConnection = connectionstring };
-                var updatedConfigJson = JsonSerializer.Serialize(deserializedconfig, new JsonSerializerOptions
-                {
-                    WriteIndented = true
-                }); ;
+                var updatedConfigJson = JsonSerializer.Serialize(deserializedconfig, new JsonSerializerOptions{ WriteIndented = true });
                 System.IO.File.WriteAllText("appsettings.json", updatedConfigJson);
             }
         }
