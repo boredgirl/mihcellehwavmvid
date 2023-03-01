c2e4fa2e9083d335e85e319f6778b33fdb031122
boredgirl <hwavmvid.17@gmail.com>
2023-03-01 17:09:09 +0100

    updated package things in navigation

---
+++ Mihcelle.Hwavmvid/Server/Controllers/Modulepackagecontroller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mihcelle.Hwavmvid.Modules.Htmleditor;
+using Mihcelle.Hwavmvid.Shared.Models;
+
+namespace Mihcelle.Hwavmvid.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Modulepackagecontroller : ControllerBase
+    {
+
+        public Applicationdbcontext applicationdbcontext { get; set; }
+        public Modulepackagecontroller(Applicationdbcontext applicationdbcontext)
+        {
+            this.applicationdbcontext = applicationdbcontext;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<List<Applicationmodulepackage>> Get()
+        {
+            var items = await this.applicationdbcontext.Applicationmodulepackages.Where(item => !string.IsNullOrEmpty(item.Name)).ToListAsync();
+            return items;
+        }
+
+    }
+}
