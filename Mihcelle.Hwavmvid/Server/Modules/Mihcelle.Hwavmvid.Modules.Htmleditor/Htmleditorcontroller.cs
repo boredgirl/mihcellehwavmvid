230f991ee340d4922b1c956e63bed9fe9c43806a
boredgirl <hwavmvid.17@gmail.com>
2023-03-03 06:13:59 +0100

    refacotred htmleditor for simple usage

--- Mihcelle.Hwavmvid/Server/Modules/Mihcelle.Hwavmvid.Modules.Htmleditor/Htmleditorcontroller.cs
+++ Mihcelle.Hwavmvid/Server/Modules/Mihcelle.Hwavmvid.Modules.Htmleditor/Htmleditorcontroller.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mihcelle.Hwavmvid.Modules.Htmleditor
 {
@@ -9,7 +11,46 @@ namespace Mihcelle.Hwavmvid.Modules.Htmleditor
     public class Htmleditorcontroller : ControllerBase
     {
 
+        public Applicationdbcontext applicationdbcontext { get; set; }
 
+        public Htmleditorcontroller(Applicationdbcontext applicationdbcontext) 
+        {
+            this.applicationdbcontext = applicationdbcontext;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{moduleid}")]
+        public async Task<Applicationhtmleditor> Get(string moduleid)
+        {
+            var editor = await this.applicationdbcontext.Applicationhtmleditors.FirstOrDefaultAsync(item => item.Moduleid == moduleid);
+            return editor;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task Post([FromBody] Applicationhtmleditor editor)
+        {
+
+            var itemexist = this.applicationdbcontext.Applicationhtmleditors.FirstOrDefault(item => item.Id == editor.Id);
+            if(itemexist != null)
+            {
+                itemexist.Htmlstring = editor.Htmlstring;
+                this.applicationdbcontext.Applicationhtmleditors.Update(itemexist);
+                this.applicationdbcontext.SaveChanges();
+            }
+            else
+            {
+                var item = new Applicationhtmleditor()
+                {
+                    Moduleid = editor.Moduleid,
+                    Htmlstring = editor.Htmlstring,
+                    Createdon = DateTime.Now,
+                };
+
+                this.applicationdbcontext.Applicationhtmleditors.Add(item);
+                this.applicationdbcontext.SaveChanges();
+            }
+        }
 
     }
 
