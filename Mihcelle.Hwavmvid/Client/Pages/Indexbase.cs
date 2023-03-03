c64f58720f85b727e5fba75b5f8493e10684501d
boredgirl <hwavmvid.17@gmail.com>
2023-03-03 03:49:20 +0100

    updated indexbase

--- Mihcelle.Hwavmvid/Client/Pages/Indexbase.cs
+++ Mihcelle.Hwavmvid/Client/Pages/Indexbase.cs
@@ -5,28 +5,32 @@ using Mihcelle.Hwavmvid.Client.Container;
 
 namespace Mihcelle.Hwavmvid.Client.Pages
 {
-    public class Indexbase : Mainlayoutbase, IDisposable
+    public class Indexbase : ComponentBase, IDisposable
     {
 
+        [Inject] public IHttpClientFactory ihttpclientfactory { get; set; }
+        [Inject] public NavigationManager navigationmanager { get; set; }
+        [Inject] public Applicationprovider applicationprovider { get; set; }
 
         [Parameter] 
-        public string? _contextpageurlpath { get; set; }
+        public string _contextpageurlpath { get; set; }
+        private string _contextpagealreadyrequested { get; set; }
 
         private const string frontpage = "mihcellehwavmvid_techonologies";
 
         protected override async Task OnInitializedAsync()
         {
-            this.navigationmanager.LocationChanged += async (obj, ev) => await this.Getcontextpage();
+            this.navigationmanager.LocationChanged += async (obj, ev) => await this.Getcontextpage(this._contextpageurlpath);
             await base.OnInitializedAsync();
         }
 
         protected override async Task OnParametersSetAsync()
         {
+
             if (string.IsNullOrEmpty(this._contextpageurlpath))
-            {
                 this._contextpageurlpath = frontpage;
-                await this.Getcontextpage();
-            }                
+
+            await this.Getcontextpage(this._contextpageurlpath);
 
             await base.OnParametersSetAsync();
         }
@@ -44,14 +48,15 @@ namespace Mihcelle.Hwavmvid.Client.Pages
             await base.OnAfterRenderAsync(firstRender);
         }
 
-        private async Task Getcontextpage()
+        private async Task Getcontextpage(string path)
         {
             try
             {
-                if (!string.IsNullOrEmpty(this._contextpageurlpath))
+                if (path != this._contextpagealreadyrequested)
                 {
+                    this._contextpagealreadyrequested = path;
                     var client = this.ihttpclientfactory?.CreateClient("Mihcelle.Hwavmvid.ServerApi.Unauthenticated");
-                    this.applicationprovider._contextpage = await client.GetFromJsonAsync<Applicationpage>(string.Concat("api/page/", this._contextpageurlpath));
+                    this.applicationprovider._contextpage = await client.GetFromJsonAsync<Applicationpage>(string.Concat("api/page/", path));
                     this.StateHasChanged();
                     this.applicationprovider._contextpagechanged();
                 }
@@ -61,7 +66,7 @@ namespace Mihcelle.Hwavmvid.Client.Pages
 
         public void Dispose()
         {
-            this.navigationmanager.LocationChanged -= async (obj, ev) => await this.Getcontextpage();
+            this.navigationmanager.LocationChanged -= async (obj, ev) => await this.Getcontextpage(this._contextpageurlpath);
         }
 
     }
