b315d8605120f7a4e9f452d5e30651df0f671460
boredgirl <hwavmvid.17@gmail.com>
2023-04-11 11:05:45 +0200

    updated jsinterop path

--- BlazorDraggableList/BlazorDraggableListService.cs
+++ BlazorDraggableList/BlazorDraggableListService.cs
@@ -24,7 +24,7 @@ namespace BlazorDraggableList
 
         public async Task InitDraggableList(string elementId)
         {
-            this.Module = await this.JSRuntime.InvokeAsync<IJSObjectReference>("import", "/_content/Mihcelle.Hwavmvid.BlazorDraggableList/blazordraggablelistjsinterop.js");
+            this.Module = await this.JSRuntime.InvokeAsync<IJSObjectReference>("import", "/_content/BlazorDraggableList/blazordraggablelistjsinterop.js");
             this.DraggableListMap = await this.Module.InvokeAsync<IJSObjectReference>("initblazordraggablelist", this.dotNetObjectReference, elementId);
         }
 
